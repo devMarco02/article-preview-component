908aa4caa68ceff22ccd97f431d1b08207aebe4f
devMarco.02 <devMarco.02@gmail.com>
2021-04-17 14:51:26 +0800

    Refactored to BEM methodology

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // toggle share button
 
-const shareBtn = document.querySelector(".share-button");
-const footer = document.querySelector(".card-footer");
-const shareSvg = document.querySelector(".share");
+const shareBtn = document.querySelector(".card__share-button");
+const footer = document.querySelector(".card__footer");
+const shareIcon = document.querySelector(".card__share-icon");
+const shareLinks = document.querySelector(".card__share-links");
 
 shareBtn.onclick = () => {
-  footer.classList.toggle("active");
-  shareSvg.classList.toggle("clicked");
+  footer.classList.toggle("card__footer--active");
+  shareIcon.classList.toggle("card__share-icon--active");
+  shareBtn.classList.toggle("card__share-button--active");
+  shareLinks.classList.toggle("card__share-links--active");
 };
 
 // social media share
 
-const facebook = document.querySelector(".fb-btn");
-const twitter = document.querySelector(".twitter-btn");
-const pinterest = document.querySelector(".pinterest-btn");
+const facebook = document.querySelector(".card__media-link--fb");
+const twitter = document.querySelector(".card__media-link--twitter");
+const pinterest = document.querySelector(".card__media-link--pinterest");
 
 const init = () => {
   const img = document.createElement("img");
